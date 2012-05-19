22831a532a1430bb63bd90df86ebd91cb4780157
Juan-Manuel Fluxa <fluxa@fluxa.local>
2012-05-19 19:07:22 +0800

    Initial Commit

---
+++ SCDLifeStyle/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SCDLifeStyle
+//
+//  Created by Juan Fluxa on 5/19/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
