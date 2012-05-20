3812e2a8fc9929da608d81639997115013797749
Anna Grueter <ab0lin@hotmail.com>
2012-05-20 21:12:49 +0800

    all

---
+++ SCDLifeStyle/ChallengesViewController.h
@@ -0,0 +1,13 @@
+//
+//  ChallengesViewController.h
+//  SCDLifeStyle
+//
+//  Created by Anna Grueter on 5/20/12.
+//  Copyright (c) 2012 reigndesign. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface ChallengesViewController : BaseViewController
+
+@end
