ce6c0833335354ba3b9b9b60f70c4bc4949b326c
fluxa <jmfluxa@gmail.com>
2012-05-21 00:02:33 +0800

    added bar components

---
+++ SCDLifeStyle/Bar.h
@@ -0,0 +1,16 @@
+//
+//  Bar.h
+//  SCDLifeStyle
+//
+//  Created by Juan Fluxa on 5/20/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Bar : UIImageView
+
+@property(nonatomic,assign) float percent;
+@property(nonatomic,strong) UIView *topPiece;
+
+@end
