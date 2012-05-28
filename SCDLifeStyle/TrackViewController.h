6b68c78a0f64a641d2d74885e3dfcfc58e7ac327
fluxa <jmfluxa@gmail.com>
2012-05-29 00:44:54 +0800

    added calendar and TapkuLibrary

---
+++ SCDLifeStyle/TrackViewController.h
@@ -0,0 +1,14 @@
+//
+//  TrackViewController.h
+//  SCDLifeStyle
+//
+//  Created by Juan Fluxa on 5/28/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <TapkuLibrary/TapkuLibrary.h>
+
+@interface TrackViewController : TKCalendarMonthViewController
+
+@end
