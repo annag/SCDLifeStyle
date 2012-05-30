d99e8110fa16d202fa1d8179106759c60a93ce55
fluxa <jmfluxa@gmail.com>
2012-05-30 14:25:02 +0800

    hocked up calendar

--- SCDLifeStyle/TrackViewController.m
+++ SCDLifeStyle/TrackViewController.m
@@ -7,7 +7,79 @@
 //
 
 #import "TrackViewController.h"
+#import "Util.h"
+#import "Day.h"
+#import "AddDayViewController.h"
+
+@interface TrackViewController () <TKCalendarMonthViewDelegate,TKCalendarMonthViewDataSource>
+
+@property(nonatomic,strong) NSMutableArray *dataArray;
+@property(nonatomic,strong) NSMutableArray *dataDict;
+
+@end
 
 @implementation TrackViewController
+
+@synthesize dataArray,dataDict;
+
+- (void) viewDidLoad
+{
+    [super viewDidLoad];
+    
+    [self.view setBackgroundColor:[UIColor blackColor]];
+    self.monthView.frame = CGRectMake(0, 0, self.monthView.frame.size.width, self.monthView.frame.size.height);
+}
+
+- (void) viewDidAppear:(BOOL)animated
+{
+    if (animated) {
+        [self.monthView reload];
+    }
+    [super viewDidAppear:animated];
+}
+
+- (void) prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    if ([segue.identifier isEqualToString:@"AddDay"]) 
+    {
+        AddDayViewController *advc = [segue destinationViewController];
+        advc.selectedDate = (NSDate*)sender;
+    }
+}
+
+#pragma mark TKCalendar delegate
+- (NSArray*) calendarMonthView:(TKCalendarMonthView*)monthView marksFromDate:(NSDate*)startDate toDate:(NSDate*)lastDate
+{
+    self.dataArray = [NSMutableArray array];
+    self.dataDict = [NSMutableArray array];
+    NSDate *d = startDate;
+	while(YES)
+    {
+		Day *day = [[Util instance] getDayFromDate:d];
+        
+        if (day != nil) 
+        {
+            [self.dataArray addObject:[NSNumber numberWithBool:YES]];
+
+        }
+        else 
+        {
+            [self.dataArray addObject:[NSNumber numberWithBool:NO]];
+        }
+        
+        		
+		TKDateInformation info = [d dateInformationWithTimeZone:[NSTimeZone timeZoneForSecondsFromGMT:0]];
+		info.day++;
+		d = [NSDate dateFromDateInformation:info timeZone:[NSTimeZone timeZoneForSecondsFromGMT:0]];
+		if([d compare:lastDate]==NSOrderedDescending) break;
+	}
+    
+    return dataArray;
+}
+
+- (void) calendarMonthView:(TKCalendarMonthView*)monthView didSelectDate:(NSDate*)date
+{
+    [self performSegueWithIdentifier:@"AddDay" sender:date];
+}
 
 @end
--- SCDLifeStyle/Util.m
+++ SCDLifeStyle/Util.m
@@ -8,9 +8,38 @@
 
 #import "Util.h"
 
+static Util *instance = nil;
+
 @implementation Util
+@synthesize managedObjectContext;
+
++ (Util*)instance
+{
+    @synchronized(self)
+    {
+    	if (instance == nil)
+    		instance = [[Util alloc] init];
+    }
+    return instance;
+}
+
++ (id)allocWithZone:(NSZone *)zone {
+    @synchronized(self) {
+        if (instance == nil) {
+            instance = [super allocWithZone:zone];
+            return instance;  // assignment and return on first allocation
+        }
+    }
+    return nil; // on subsequent allocation attempts return nil
+}
+
+- (id)copyWithZone:(NSZone *)zone
+{
+    return self;
+}
 
 
+//static
 + (NSString*) dayIdFromDate:(NSDate*)date
 {
     NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
@@ -41,5 +70,20 @@
     return dayId;
 }
 
+//public
+- (Day*) getDayFromDate:(NSDate *)date
+{
+    NSString *dateId = [Util dayIdFromDate:date];
+    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"day_id == %@",dateId];
+    NSFetchRequest *request = [NSFetchRequest fetchRequestWithEntityName:@"Day"];
+    [request setPredicate:predicate];
+    NSError *error = nil;
+    NSArray *a = [self.managedObjectContext executeFetchRequest:request error:&error];
+    if ([a count] > 0 && error == nil) 
+    {
+        return (Day*)[a objectAtIndex:0];
+    }
+    return nil;
+}
 
 @end
