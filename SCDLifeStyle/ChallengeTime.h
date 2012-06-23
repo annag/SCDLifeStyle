9acdfeaaeafabed49b8b32af03dd12d0c8ab8e4d
fluxa <jmfluxa@gmail.com>
2012-06-23 13:44:50 +0800

    added new views for challenge

---
+++ SCDLifeStyle/ChallengeTime.h
@@ -0,0 +1,13 @@
+//
+//  ChallengeTime.h
+//  SCDLifeStyle
+//
+//  Created by Juan Fluxa on 6/23/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChallengeTime : UIView
+
+@end
